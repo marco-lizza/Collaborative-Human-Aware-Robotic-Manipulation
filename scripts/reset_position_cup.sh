fb553be314db8e112b47f3b1c485bdff185c8f0d
Marco Lizza <marcolizza1@gmail.com>
2026-03-21 16:50:58 +0100

    Introdotto il modello 3d del case che forza la tazza in posizione

--- scripts/reset_position_cup.sh
+++ scripts/reset_position_cup.sh
@@ -2,5 +2,5 @@
 ign service -s /world/worldCobotta/set_pose \
 --reqtype ignition.msgs.Pose \
 --reptype ignition.msgs.Boolean \
---req 'name: "Cup", position: {x: -9.36, y: -1.02, z: 1.04}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 0.0}' \
+--req 'name: "Cup", position: {x: -9.36, y: -1.02, z: 1.15}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 0.0}' \
 --timeout 2000
