83990455712e0b8c25a3fd384d3b03da3d552468
Marco Lizza <marcolizza1@gmail.com>
2026-03-22 21:34:54 +0100

    Introdotto il modello 3d del case che forza il piatto il posizione + nuovo modello 3d per il piatto

--- scripts/reset_position_dish.sh
+++ scripts/reset_position_dish.sh
@@ -2,5 +2,5 @@
 ign service -s /world/worldCobotta/set_pose \
  --reqtype ignition.msgs.Pose \
  --reptype ignition.msgs.Boolean \
- --req 'name: "Dish", position: {x: -9.36, y: -1.38, z: 1.04}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 0.0}' \
- --timeout 2000 
+ --req 'name: "Dish", position: {x: -9.36, y: -1.38, z: 1.40}, orientation: {x: 0.0, y: 0.0, z: -0.552554, w: 0.833486}' \
+ --timeout 2000
