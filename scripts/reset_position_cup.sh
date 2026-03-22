e2c801aba23e946f144a4a9d441fda14662454b3
Marco Lizza <marcolizza1@gmail.com>
2026-03-22 08:57:40 +0100

    Risolto il problema di CPU satura dovuta alle mesh della tazza e del contenitore ( introdotti nuovi modelli )

--- scripts/reset_position_cup.sh
+++ scripts/reset_position_cup.sh
@@ -2,5 +2,5 @@
 ign service -s /world/worldCobotta/set_pose \
 --reqtype ignition.msgs.Pose \
 --reptype ignition.msgs.Boolean \
---req 'name: "Cup", position: {x: -9.36, y: -1.02, z: 1.15}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 0.0}' \
+--req 'name: "Cup", position: {x: -9.36, y: -1.02, z: 1.14}, orientation: {x: 0.0, y: 0.0, z: -0.1986693, w: 0.9800666}' \
 --timeout 2000
